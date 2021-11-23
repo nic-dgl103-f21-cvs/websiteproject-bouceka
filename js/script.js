fe78efe3f103ded42d3cf19fb48a2f2ef416f296
bouceka <adamm01b@gmail.com>
2021-11-23 11:12:37 -0800

    web-project: Chore: Delete redundency + fix naming convention

--- js/script.js
+++ js/script.js
@@ -1,25 +1,16 @@
-///////////////////////////////////////////////////////////
-// Make mobile navigation work
-
-const btnNavEl = document.getElementById('nav-icon');
-const headerEl = document.getElementsByClassName('collapsed-menu');
-const listEl = document.getElementsByClassName('collapsed-list');
+const btnNavEl = document.getElementById('nav-icon'); // navigation icon
+const headerEl = document.getElementsByClassName('collapsed-menu'); // navigation list
 
 btnNavEl.addEventListener('click', function () {
   if (headerEl[0].classList[1] === 'no-active') {
+		// open the navigation
     headerEl[0].classList.add('is-active');
-    listEl[0].classList.add('is-active');
     headerEl[0].classList.remove('no-active');
-    listEl[0].classList.remove('no-active');
-	btnNavEl.classList.add('open')
+    btnNavEl.classList.add('open');
   } else {
+		// close the navigation
     headerEl[0].classList.remove('is-active');
-    listEl[0].classList.remove('is-active');
-	btnNavEl.classList.remove('open')
+    btnNavEl.classList.remove('open');
     headerEl[0].classList.add('no-active');
-    listEl[0].classList.add('no-active');
-
   }
 });
-
-
